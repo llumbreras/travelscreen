ea7ddc85c4d312f53a5da3e0f4c173c8fd0ff8dd
L Lumbreras <llumbreras@gmail.com>
2014-01-28 15:27:39 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'bootstrap-sass'
+gem 'bootstrap_form'
+gem 'coffee-rails'
+gem 'rails'
+gem 'haml-rails'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'jquery-rails'
+gem 'bcrypt-ruby'
+
+group :development do
+  gem 'sqlite3'
+  gem 'pry'
+  gem 'pry-nav'
+  gem 'thin'
+  gem "better_errors"
+  gem "binding_of_caller"
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+end
+
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Travelflix::Application.config.secret_key_base = '54b6cab7b147404a4bdab53efa49e7234cfedea591526750742a6b5c9989726db0c4675184161efca70c4284c972ebb6cdcb307c5b65b61b64f38891c801c0f7'
