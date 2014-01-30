4054c889e1dc53f075083e3ee9769a3a882b5be1
L Lumbreras <llumbreras@gmail.com>
2014-01-29 18:32:52 -0800

    Add seed to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+# Video.create(title: , description: , small_cover_url: , large_cover_url: )
+
+Video.create(title: "About Time" , description: "Travel in time and change what happens", small_cover_url: "/tmp/about_time_small.png", large_cover_url: "/tmp/about_time_large.png" )
+Video.create(title: "Up", description: "Sets out to fulfill lifelong dream", small_cover_url: "/tmp/up_small.png", large_cover_url: "/tmp/up_large.png" )
+Video.create(title: "Rio", description: "Takes off on an adventure", small_cover_url: "/tmp/rio_small.png", large_cover_url: "/tmp/rio_large.png")
+Video.create(title: "Midnight in Paris", description: "Going back to the 1920s every day at midnight", small_cover_url: "/tmp/midnight_in_paris_small.png", large_cover_url: "/tmp/midnight_in_paris_large.png")
+Video.create(title: "When in Rome", description: "A whirlwind trip to Rome", small_cover_url: "/tmp/when_in_rome_small.png", large_cover_url: "/tmp/when_in_rome_large.png")
+Video.create(title: "Last Stop for Paul", description: "Travel to the Full Moon party in Thailand", small_cover_url: "/tmp/last_stop_for_paul_small.png", large_cover_url: "/tmp/last_stop_for_paul_large.png")
+Video.create(title: "New York", description: "Stories set in New York", small_cover_url: "/tmp/new_york_i_love_you_small.png", large_cover_url: "/tmp/new_york_i_love_you_large.png")
+Video.create(title: "Paris", description: "Stories set in Paris", small_cover_url: "paris_je_taime_small.png", large_cover_url: "/tmp/paris_je_taime_large.png")
+Video.create(title: "To Rome with Love", description: "The lives of visitors and residents in Rome", small_cover_url: "/tmp/to_rome_with_love_small.png", large_cover_url: "/tmp/to_rome_with_love_large.png")
+Video.create(title: "Secret Life of Wlater Mitty", description: "A day-dreamer escapes his anonymous life", small_cover_url: "/tmp/secret_life_walter_mitty_small.png", large_cover_url: "/tmp/secret_life_walter_mitty_large.png")
+Video.create(title: "Best Exotic Marigold Hotel", description: "Retirees travel to India", small_cover_url: "/tmp/best_exotic_marigold_hotel_small.png", large_cover_url: "/tmp/best_exotic_marigold_hotel_large.png")
+
+
