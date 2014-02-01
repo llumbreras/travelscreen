03889bca3e6b359f414b90b359485d20538a3981
L Lumbreras <llumbreras@gmail.com>
2014-02-01 11:13:38 -0800

    Modify test condition

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,11 +2,5 @@ require 'spec_helper'
 
 describe Category do
   it { should validate_presence_of(:name) }
-  
-  it "has many videos" do
-    comedy = Category.create(name: 'Comedy')
-    up = Video.create(title: 'Up', description: 'A travel adventure', category: comedy)
-    rio = Video.create(title: 'Rio', description: 'A Rio adventure', category: comedy)
-    expect(comedy.videos).to eq([up, rio])
-  end
+  it { should have_many(:videos) } 
 end
