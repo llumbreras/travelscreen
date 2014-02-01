be6aa23caffa5af12048db00c6862fbba91bbdaf
L Lumbreras <llumbreras@gmail.com>
2014-02-01 11:09:19 -0800

    Modify test to use shoulda-matchers

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,25 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: 'Up', description: 'A travel adventure')
-    video.save
-    expect(Video.first).to eq(video)
-  end
-  it "belongs to category" do
-    comedy = Category.create(name: 'Comedy')
-    up = Video.create(title: 'Up', description: 'A travel adventure', category: comedy)
-    rio = Video.create(title: 'Rio', description: 'A travel adventure', category: comedy)
-    expect(comedy.videos).to eq([up, rio])
-  end
-  it "validates if title is present" do
-    video = Video.new(title: '', description: 'A travel adventure')
-    video.save
-    expect(Video.count).to eq(0)
-  end
-  it "validates if description is present" do
-    video = Video.new(title: 'Up', description: '')
-    video.save
-    expect(Video.count).to eq(0)
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
